506e34a4ae2aa64af4505006e2f8466382b6880f
jruipinto <44852805+jruipinto@users.noreply.github.com>
2019-12-04 19:20:30 +0000

    refactor:  ignored dist folder in .gitignore

--- dist/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var modem_1 = require("./modem");
-modem_1.modem.init();
-modem_1.modem.sendTextMessage(910138725, 'teste de modem novo');
